c70de6b9d8f0748c21d5249a373226da58ff18a7
Morenike Oyewole <65955563+mo-renike@users.noreply.github.com>
2022-09-04 21:27:30 +0100

    Update FetchData.js

--- src/functions/FetchData.js
+++ src/functions/FetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    'X-RapidAPI-Key': 'b5955c940amsh9ba067ff07dbc5ap17f2abjsn2acd75e2d6af',
+    'X-RapidAPI-Key': '8b6089ebddmshc744444415dfccbp158bf5jsnc56929b628e8',
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
